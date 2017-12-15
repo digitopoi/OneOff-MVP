9b8ada75f4e855ccebbf9ca01152e596ef4bf5ce
digitopoi <hubo85@gmail.com>
2017-12-15 11:46:04 -0600

    Add login and redirect

--- OneOff.Web.SPA/src/app/services/auth.service.ts
+++ OneOff.Web.SPA/src/app/services/auth.service.ts
@@ -1,13 +1,17 @@
 import { Token } from './../models/Token';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RegisterUser } from './../models/RegisterUser';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
 
 const Api_Url = 'http://localhost:63577';
 @Injectable()
 export class AuthService {
 
-  constructor(private _http: HttpClient) { }
+  constructor(
+    private _http: HttpClient,
+    private _router: Router) { }
 
   register(regUserData: RegisterUser) {
     return this._http.post(`${Api_Url}/api/Account/Register`, regUserData);
@@ -17,8 +21,18 @@ export class AuthService {
     const string =
       `grant_type=password&username=${encodeURI(loginInfo.email)}&password=${encodeURI(loginInfo.password)}`;
 
-    return this._http.post(`@{Api_Url}/token`, string).subscribe( (token: Token) => {
+    return this._http.post(`${Api_Url}/token`, string).subscribe( (token: Token) => {
       localStorage.setItem('id_token', token.access_token);
-    })
+      this._router.navigate(['/gigs']);
+    });
   }
+
+  currentUser(): Observable<Object> {
+    if (!localStorage.getItem('id_token')) { return new Observable(observer => observer.next(false)); }
+
+    const authHeader = new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
+
+    return this._http.get(`${Api_Url}/api/Account/UserInfo`, { headers: authHeader });
+  }
+
 }
